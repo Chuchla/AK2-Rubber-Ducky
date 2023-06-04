9d8ebcd940abded47a545230872d29b89fbbc8f9
Jakub Chuchla <128312745+Chuchla@users.noreply.github.com>
2023-06-04 23:42:00 +0200

    Update - comments
    Comments added to the code

--- Animation.ino
+++ Animation.ino
@@ -5,42 +5,25 @@ void setup() {
   // put your setup code here, to run once:
     DigiKeyboard.delay(1000);
     DigiKeyboard.sendKeyStroke(0,0);
-    DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT);
+    DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT); //Click windows key
     DigiKeyboard.delay(1000);
     String toWrite = "noTepad";
       for(char i = 0 ; i < toWrite.length(); i++){
-      if(toWrite.charAt(i) == ' '){
-        DigiKeyboard.sendKeyStroke(KEY_SPACE);
-        continue;
-      }
-      else if(toWrite.charAt(i) == '\n'){
-        DigiKeyboard.sendKeyStroke(KEY_ENTER);
-        continue;
-      }
-
-      else toWrite.setCharAt(i, toWrite.charAt(i)+8);
+      toWrite.setCharAt(i, toWrite.charAt(i)+8);
     }
-
-    DigiKeyboard.println(toWrite);
-    DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(1500);
+    DigiKeyboard.println(toWrite); //Write notepad
+    DigiKeyboard.sendKeyStroke(KEY_ENTER); //Open notepad
+    DigiKeyboard.delay(200);
     
 }    
 
 void loop() {
   // put your main code here, to run repeatedly:
-    DigiKeyboard.delay(500);  
-      String toWrite = "Losowy TeksT\n";
-
+    DigiKeyboard.delay(1000);  
+    String toWrite = "Hello World\n";
     for(char i = 0 ; i < toWrite.length(); i++){
-      if(toWrite.charAt(i) == '\n'){
-        DigiKeyboard.sendKeyStroke(KEY_ENTER);
-        continue;
-      }
-
-      else toWrite.setCharAt(i, toWrite.charAt(i)+8);
+      toWrite.setCharAt(i, toWrite.charAt(i)+8);
     }
-    
-    DigiKeyboard.println(toWrite);
+    DigiKeyboard.println(toWrite); //Write Hello World in a loop
 }
 
